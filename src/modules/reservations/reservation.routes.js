a09f2ea158eb7e4de36c98ba6067454d6985337f
Gab <gabionian7@gmail.com>
2026-04-02 01:22:50 +0800

    add_reservation

--- src/modules/reservations/reservation.routes.js
+++ src/modules/reservations/reservation.routes.js
@@ -12,28 +12,12 @@ const allowRoles = (...roles) => (req, res, next) => {
 
 router.use(authenticate);
 
-// GET all reservations for user's branch (no branchId required)
-router.get('/', allowRoles('OWNER', 'MANAGER', 'CASHIER'), ctrl.listReservationsForBranch);
-
-// GET all reservations for specified branch
 router.get('/:branchId', allowRoles('OWNER', 'MANAGER', 'CASHIER'), ctrl.listReservations);
-
-// GET specific reservation
 router.get('/detail/:id', allowRoles('OWNER', 'MANAGER', 'CASHIER'), ctrl.getReservation);
-
-// CREATE new reservation
 router.post('/', allowRoles('OWNER', 'MANAGER', 'CASHIER'), ctrl.createReservation);
-
-// UPDATE reservation
 router.patch('/:id', allowRoles('OWNER', 'MANAGER', 'CASHIER'), ctrl.updateReservation);
-
-// UPDATE status
 router.patch('/:id/status', allowRoles('OWNER', 'MANAGER', 'CASHIER'), ctrl.updateStatus);
-
-// MARK as paid
 router.patch('/:id/pay', allowRoles('OWNER', 'MANAGER', 'CASHIER'), ctrl.markPaid);
-
-// DELETE reservation
 router.delete('/:id', allowRoles('OWNER', 'MANAGER'), ctrl.deleteReservation);
 
 module.exports = router;
